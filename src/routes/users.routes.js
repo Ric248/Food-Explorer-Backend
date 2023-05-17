9d010a65d655fc5b82bf39094b27769c3a907344
Ric248 <ric248@gmail.com>
2023-05-17 16:41:55 -0300

    [feat] Adicionando o UsersController

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 
+const UsersController = require("../controllers/UsersController")
+
 const usersRoutes = Router();
+
+// Instanciando as classes
+const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 
