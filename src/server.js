73d2a8c6a0e6aee9915292464ef4cd207d70cd91
Ric248 <ric248@gmail.com>
2023-05-22 09:19:25 -0300

    [feat] Adicionando a classe de erros

--- src/server.js
+++ src/server.js
@@ -8,4 +8,4 @@ app.use(express.json())
 app.use(routes);
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));  
