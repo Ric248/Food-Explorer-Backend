cbd35eb7d1882522b8f74525679cddebd496b069
Ric248 <ric248@gmail.com>
2023-06-05 15:00:22 -0300

    [feat] Fazendo pequenos ajustes

--- src/database/knex/migrations/20230522195603_createDishes.js
+++ src/database/knex/migrations/20230522195603_createDishes.js
@@ -2,7 +2,6 @@ exports.up = knex => knex.schema.createTable("dishes", table => {
   table.increments("id");
   table.text("title");
   table.text("description");
-  table.text("category");
   table.text("price");
   table.varchar("image");
 });
