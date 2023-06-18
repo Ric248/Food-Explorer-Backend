fa39349412b57aa8fe0315d791df6941a8e568ae
Ric248 <ric248@gmail.com>
2023-06-18 16:21:25 -0300

    [fix] Fazendo pequenos ajustes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
-const multer = require('multer');
-const uploadConfig = require('../configs/upload');
 
 const UsersController = require('../controllers/UsersController')
 
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const usersRoutes = Router();
-const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
